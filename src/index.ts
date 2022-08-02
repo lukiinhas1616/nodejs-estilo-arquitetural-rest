f2699c8a94fb09179c9ea3ab9646545fc22dc090
lukiinhas1616 <37242437+lukiinhas1616@users.noreply.github.com>
2022-08-01 21:35:53 -0300

    creating a listener and automating updates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import  express,{Request, Response, NextFunction}  from "express";
+
+const app = express();
+
+app.get("/status", (req: Request, res: Response, next:NextFunction) => {
+    res.status(200).send({status: "🟢 API operacional" })
+});
+
+app.listen(3000,()=>{
+    console.log("Aplicação iniciada em porta 3000")
+})
